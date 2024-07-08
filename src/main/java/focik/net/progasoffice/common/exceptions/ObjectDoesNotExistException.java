3c8c0d39bb71e1a66a60915192fec010f92c34e0
Tomek <tkrutowski@gmail.com>
2024-07-08 22:46:15 +0200

    v 1002 change colors

---
+++ src/main/java/focik/net/progasoffice/common/exceptions/ObjectDoesNotExistException.java
@@ -0,0 +1,7 @@
+package focik.net.progasoffice.common.exceptions;
+
+public class ObjectDoesNotExistException extends RuntimeException {
+    public ObjectDoesNotExistException(String message) {
+        super(message);
+    }
+}
